61f5e297945deacd5c2a6ad28280ff1a468a3b6e
bdownz93 <149603742+bdownz93@users.noreply.github.com>
2024-11-01 16:48:04 -0400

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  // Change from 'export' to use server-side features
+  output: 'standalone',
   images: { 
     unoptimized: true,
     remotePatterns: [
